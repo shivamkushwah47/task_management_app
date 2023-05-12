2f5ab1107c28db0e0bf3494f4069c7e17ffb281f
shivam kushwah <shivakushwah760@gmail.com>
2023-05-12 23:36:48 +0530

    check push

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+
       debugShowCheckedModeBanner: false,
       initialRoute: Routes.splash ,
       getPages: allpages,

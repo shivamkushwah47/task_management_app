004a2d678d464900c144c7f30e79f4caabb557e1
shivam kushwah <shivakushwah760@gmail.com>
2023-05-12 23:28:41 +0530

    first commit

---
+++ lib/core/components/custombutton.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:task_management_app/core/global/colors.dart';
+
+class CustomButton extends GetView {
+  final String title;
+  final VoidCallback onPress;
+
+  const CustomButton({super.key, required this.title, required this.onPress});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: Get.width,
+      child: MaterialButton(
+        onPressed: onPress,
+        color: GlobalColor.themecolor,
+        shape: BeveledRectangleBorder(),
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Text(
+            title,
+            style: TextStyle(
+              color: Colors.white,
+              letterSpacing: 1,
+              fontWeight: FontWeight.w600,
+              fontSize: Get.height * 0.02,
+              //    fontFamily: CustomFonts.alata
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/core/global/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class GlobalColor{
+  static MaterialColor customMaterialColor = Colors.blue;
+  static Color themecolor = Colors.indigoAccent;
+  static Color cancelbut = Colors.white;
+
+}
